7feea24915c62ab9238ad91962c785629eba19f5
NoSignificantHarassment37 <mateoatehortua1111@gmail.com>
2025-10-26 22:50:38 -0500

    log url

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import { PrismaClient } from '@prisma/client';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
+console.log(`DATABASE_URL:${process.env.DATABASE_URL}`);
 // ----------------------
 // Prisma Client
 // ----------------------
